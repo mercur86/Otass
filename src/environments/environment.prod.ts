ee12b66bd6fef18971a3f4b72a8e37f2d18161e9
mercur86 <67831451+mercur86@users.noreply.github.com>
2023-12-13 09:18:47 -0500

    merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   TASA: 0.0124,
   VERSIONAPP:'2.8',
- ROOTAPI:'https://sisgeco.epsgrau.pe/SISGECO/servicioWeb/',
+   ROOTAPI:'https://sisgeco.epsgrau.pe/SISGECO/servicioWeb/',
    //ROOTAPI:'https://pruebas.epsgrau.pe/SISGECO/servicioWeb/',
   // ROOTAPI:'http://localhost:8080/SistemaComercialEPS/servicioWeb/',
   TIMESESION:1800
