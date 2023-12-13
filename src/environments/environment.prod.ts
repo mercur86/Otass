fe2c3372e8d4ad45589aa1579b359cfed36683dd
mercur86 <67831451+mercur86@users.noreply.github.com>
2023-12-13 12:52:03 -0500

    ignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   TASA: 0.0124,
   VERSIONAPP:'2.8',
- ROOTAPI:'https://sisgeco.epsgrau.pe/SISGECO/servicioWeb/',
-   //ROOTAPI:'https://pruebas.epsgrau.pe/SISGECO/servicioWeb/',
+ //ROOTAPI:'https://sisgeco.epsgrau.pe/SISGECO/servicioWeb/',
+   ROOTAPI:'https://pruebas.epsgrau.pe/SISGECO/servicioWeb/',
   // ROOTAPI:'http://localhost:8080/SistemaComercialEPS/servicioWeb/',
   TIMESESION:2700
 
